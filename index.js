b4cce65a3c72ed6f333de6e00d4c32249d85a499
Daniela Ramirez <157253457+ablacklab@users.noreply.github.com>
2025-07-01 16:21:45 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,62 +5,83 @@ const divmsj = document.querySelector(".divmsj");
 const msjs = document.querySelector(".msjs");
 let content = " ";
 
-function esrol(mensaje){
-  if (mensaje.startsWith(" //") || mensaje.startsWith(" _//") || mensaje.endsWith("//") || mensaje.endsWith("//_") || mensaje.startsWith(" ||") || mensaje.endsWith("||") || mensaje.startsWith(" |") || mensaje.startsWith(" _•||") || mensaje.includes("\\") || mensaje.startsWith(" ;") || mensaje.endsWith("/") || mensaje.endsWith(";;") || mensaje.startsWith(` #`) || mensaje.startsWith(` _#`) || mensaje.startsWith(' &&') || mensaje.startsWith(' -') || mensaje === ' .' || mensaje.endsWith("|")){
-    return false
-  } else { 
-    return true
+function esrol(mensaje) {
+  if (
+    mensaje.startsWith(" //") ||
+    mensaje.startsWith(" _//") ||
+    mensaje.endsWith("//") ||
+    mensaje.endsWith("//_") ||
+    mensaje.startsWith(" ||") ||
+    mensaje.endsWith("||") ||
+    mensaje.startsWith(" |") ||
+    mensaje.startsWith(" _•||") ||
+    mensaje.includes("\\") ||
+    mensaje.startsWith(" ;") ||
+    mensaje.endsWith("/") ||
+    mensaje.endsWith(";;") ||
+    mensaje.startsWith(` #`) ||
+    mensaje.startsWith(` _#`) ||
+    mensaje.startsWith(" &&") ||
+    mensaje.startsWith(" -") ||
+    mensaje === " ." ||
+    mensaje.endsWith("|") ||
+    mensaje.endsWith("//•")
+  ) {
+    return false;
+  } else {
+    return true;
   }
 }
 
 function contarEmojis(mensajes) {
-  const contador = {}
+  const contador = {};
 
   for (const mensaje of mensajes) {
-  const emojis = mensaje.match(/\p{Emoji}/gu);
-  
-  if (emojis){
-    for (const emojinum of emojis) {
-      const emoji = emojinum.toLowerCase();
-      if (!contador[emoji]) {
-        contador[emoji] = 0;
+    const emojis = mensaje.match(/\p{Emoji}/gu);
+
+    if (emojis) {
+      for (const emojinum of emojis) {
+        const emoji = emojinum.toLowerCase();
+        if (!contador[emoji]) {
+          contador[emoji] = 0;
+        }
+        contador[emoji]++;
       }
-      contador[emoji]++;
     }
-  } }
+  }
   return contador;
 }
 
-function clickrol(stats, divcel){
-  console.log("clickrol")
+function clickrol(stats, divcel) {
+  console.log("clickrol");
   divcel.innerHTML = " ";
-  stats.rol.forEach(element => {
+  stats.rol.forEach((element) => {
     const mensaje = document.createElement("p");
     mensaje.textContent = element;
     divcel.appendChild(mensaje);
   });
 }
 
-function clickoff(stats, divcel){
+function clickoff(stats, divcel) {
   divcel.innerHTML = " ";
-  stats.off.forEach(element => {
+  stats.off.forEach((element) => {
     const mensaje = document.createElement("p");
     mensaje.textContent = element;
     divcel.appendChild(mensaje);
   });
 }
 
-function clickmedia(stats, divcel){
+function clickmedia(stats, divcel) {
   divcel.innerHTML = " ";
-  stats.spam.forEach(element => {
+  stats.spam.forEach((element) => {
     const mensaje = document.createElement("p");
     mensaje.textContent = element;
     divcel.appendChild(mensaje);
   });
 }
 
-function details(stats){
-  msjs.innerHTML = " "
+function details(stats) {
+  msjs.innerHTML = " ";
   msjs.classList.remove("statsmode");
 
   const nameuss = document.createElement("h2");
@@ -71,14 +92,14 @@ function details(stats){
   buttonsdiv.classList.add("buttonsdiv");
 
   const btnrol = document.createElement("button");
-  btnrol.textContent = "Rol"
-  btnrol.addEventListener(("click"), () => clickrol(stats, divcel));
+  btnrol.textContent = "Rol";
+  btnrol.addEventListener("click", () => clickrol(stats, divcel));
   const btnoff = document.createElement("button");
-  btnoff.textContent = "Offrol"
-  btnoff.addEventListener(("click"), () => clickoff(stats, divcel));
+  btnoff.textContent = "Offrol";
+  btnoff.addEventListener("click", () => clickoff(stats, divcel));
   const btnmedia = document.createElement("button");
-  btnmedia.textContent = "Media"
-  btnmedia.addEventListener(("click"), () => clickmedia(stats, divcel));
+  btnmedia.textContent = "Media";
+  btnmedia.addEventListener("click", () => clickmedia(stats, divcel));
 
   buttonsdiv.appendChild(btnrol);
   buttonsdiv.appendChild(btnoff);
@@ -88,8 +109,8 @@ function details(stats){
 
   const roldiv = document.createElement("div");
   roldiv.classList.add("mjspc");
-  roldiv.textContent = "Rol"
-  stats.rol.forEach(element => {
+  roldiv.textContent = "Rol";
+  stats.rol.forEach((element) => {
     const mensaje = document.createElement("p");
     mensaje.textContent = element;
     roldiv.appendChild(mensaje);
@@ -98,59 +119,111 @@ function details(stats){
 
   const offdiv = document.createElement("div");
   offdiv.classList.add("mjspc");
-  offdiv.textContent = "Off-rol"
-  stats.off.forEach(element => {
+  offdiv.textContent = "Off-rol";
+  stats.off.forEach((element) => {
     const mensaje = document.createElement("p");
     mensaje.textContent = element;
     offdiv.appendChild(mensaje);
   });
   msjs.appendChild(offdiv);
 
-  /*const mediadiv = document.createElement("div");
-  mediadiv.classList.add("mjspc");
-  mediadiv.textContent = "Media"
-  stats.spam.forEach(element => {
-    const mensaje = document.createElement("p");
-    mensaje.textContent = element;
-    mediadiv.appendChild(mensaje);
-  });
-  msjs.appendChild(mediadiv);*/
-
   const divcel = document.createElement("div");
   divcel.classList.add("divcel");
   msjs.appendChild(divcel);
-
-
 }
 
 function stats(allinfo) {
   msjs.innerHTML = " ";
   msjs.classList.add("statsmode");
+  msjs.classList.remove("msjs");
 
-  const top5Objects = allinfo
-  .sort((a, b) => b.rol.length - a.rol.length)
-  .slice(0, 5);
-  
-  const ussNames = top5Objects.map(obj => obj.uss);
+  msjs.innerHTML += `<h2>Top 5</h2>`;
+  const divTop = document.createElement("div");
+  const top1Div = document.createElement("div");
+  const top2Div = document.createElement("div");
+  const top3Div = document.createElement("div");
 
-  console.log(ussNames);
-  console.log(top5Objects);
+  const divTaBien = document.createElement("div");
+  const divMenos200 = document.createElement("div");
+  const divRisk = document.createElement("div");
+  const divDeath = document.createElement("div");
 
-  msjs.innerHTML = ` 
-  <h3>Top 5 con más mjs (en rol):</h3>
-  <p>1. ${top5Objects[0].uss} (${top5Objects[0].rol.length})</p>
-  <p>2. ${top5Objects[1].uss} (${top5Objects[1].rol.length})</p>
-  <p>3. ${top5Objects[2].uss} (${top5Objects[2].rol.length})</p>
-  <p>4. ${top5Objects[3].uss} (${top5Objects[3].rol.length})</p>
-  <p>5. ${top5Objects[4].uss} (${top5Objects[4].rol.length})</p>
-  `
+  let count = 0;
+
+  allinfo.forEach((element) => {
+    let usser = element.uss;
+    if (usser.includes("୧ ✿ ›")) {
+      usser = usser.split("୧ ✿ ›")[1].split("֗")[0];
+      if (usser.includes("𝒶𝓃𝒹")) {
+        usser = usser.replace("𝒶𝓃𝒹", "&");
+      }
+    }
+
+    if (count < 5) {
+      console.log(`top ${count + 1} ${usser}`);
+      if (
+        usser.includes("Ollie") ||
+        usser.includes("Dan") ||
+        usser.includes("Raqn") ||
+        usser.includes("Jazz") ||
+        usser.includes("Jaiden")
+      ) {
+        top3Div.innerHTML += `<p class="gray"><b>${element.rolnumber}</b> ${usser}</p>`;
+      } else {
+        switch (count) {
+          case 0:
+            top1Div.innerHTML += `<p class="top"><b>${element.rolnumber}</b> ${usser}</p>`;
+            break;
+          case 1:
+            top2Div.innerHTML += `<p class="top"><b>${element.rolnumber}</b> ${usser}</p>`;
+            break;
+          case 2:
+            top2Div.innerHTML += `<p class="top"><b>${element.rolnumber}</b> ${usser}</p>`;
+            break;
+          case 3:
+            top3Div.innerHTML += `<p class="top"><b>${element.rolnumber}</b> ${usser}</p>`;
+            break;
+          case 4:
+            top3Div.innerHTML += `<p class="top"><b>${element.rolnumber}</b> ${usser}</p>`;
+            break;
+          default:
+            break;
+        }
+        count++;
+      }
+    } else if (element.rolnumber < 90) {
+      divDeath.innerHTML += `<p class="death">${usser}</p>`;
+    } else if (element.rolnumber < 110) {
+      divRisk.innerHTML += `<p class="risk">${usser}</p>`;
+    } else if (element.rolnumber < 200 && usser.includes("&")) {
+      divMenos200.innerHTML += `<p class="minus">${usser}</p>`;
+    } else {
+      divTaBien.innerHTML += `<p class="kay">${usser}</p>`;
+    }
+  });
+
+  divTop.appendChild(top1Div);
+  divTop.appendChild(top2Div);
+  divTop.appendChild(top3Div);
+  msjs.appendChild(divTop);
+
+  msjs.innerHTML += `<h2>Ta bien</h2>`;
+  msjs.appendChild(divTaBien);
+
+  msjs.innerHTML += `<h2>-200</h2>`;
+  msjs.appendChild(divMenos200);
+
+  msjs.innerHTML += `<h2>90 - 110</h2>`;
+  msjs.appendChild(divRisk);
+
+  msjs.innerHTML += `<h2>ur death</h2>`;
+  msjs.appendChild(divDeath);
 }
 
 function statsHora(mjs) {
-
   const statsxHora = {
     AM: {},
-    PM: {}
+    PM: {},
   };
 
   for (const uss in mjs) {
@@ -158,9 +231,9 @@ function statsHora(mjs) {
       const hora = mensaje.hora;
 
       const mainhora = hora.split(":")[0];
-      const ampm = hora.split(' ')[1].split(' ')[1];
+      const ampm = hora.split(" ")[1].split(" ")[1];
 
-      if (ampm === "AM"){
+      if (ampm === "AM") {
         if (!statsxHora.AM[mainhora]) {
           statsxHora.AM[mainhora] = { mjs: 0, uss: {} };
         }
@@ -181,134 +254,147 @@ function statsHora(mjs) {
       }
     }
   }
-
-  console.log(statsxHora);
-};
+}
 
 function printmjs(mjs) {
-    statsHora(mjs);
+  console.log(mjs);
+  //statsHora(mjs);
 
-    const allinfo = [ ];
+  const allinfo = [];
 
-    const statsbtn = document.createElement("button");
-    statsbtn.textContent = "Stats :)";
-    statsbtn.classList.add("statsbtn");
-    divmsj.appendChild(statsbtn);
+  //HACER EL NUEVO ARRAY CON LOS MENSAJES
+  for (const [nombre, mensajes] of Object.entries(mjs)) {
+    const stats = {
+      uss: nombre,
+      rolnumber: 0,
+      bonus: 0,
+      rol: [],
+      off: [],
+      spam: [],
+    };
 
-    //HACER EL NUEVO ARRAY CON LOS MENSAJES
-    for (const [nombre, mensajes] of Object.entries(mjs)) {
-
-        const stats = {
-          uss: nombre,
-          rolnumber: 0,
-          bonus: 0,
-          rol: [],
-          off: [],
-          spam: [],
-        }
-
-        //ahora calcular los off rol
-        for (const mensajeobj of mensajes) {
-          const mensaje = mensajeobj.mensaje;
-          if(mensaje.includes("<Media omitted>") || mensaje === "" || mensaje === " null" || mensaje.includes("<View once voice message omitted>") || mensaje === " This message was deleted" || mensaje.includes("<Multimedia omitido>")){
-            stats.spam.push(mensaje);
+    //ahora calcular los off rol
+    for (const mensajeobj of mensajes) {
+      const mensaje = mensajeobj.mensaje;
+      if (
+        mensaje.includes("<Media omitted>") ||
+        mensaje === "" ||
+        mensaje === " null" ||
+        mensaje.includes("<View once voice message omitted>") ||
+        mensaje === " This message was deleted" ||
+        mensaje.includes("<Multimedia omitido>")
+      ) {
+        stats.spam.push(mensaje);
+      } else {
+        if (esrol(mensaje)) {
+          if (mensaje.length > 766) {
+            stats.rolnumber = stats.rolnumber + 15;
+            stats.bonus++;
           } else {
-            if (esrol(mensaje)){
-              if (mensaje.length > 766){
-                stats.rolnumber = stats.rolnumber + 15;
-                stats.bonus++;
-              } else {
-                stats.rolnumber++;
-              }
-              stats.rol.push(mensaje)
-            } else {
-              stats.off.push(mensaje);
-            }
+            stats.rolnumber++;
+          }
+          stats.rol.push(mensaje);
+        } else {
+          stats.off.push(mensaje);
         }
       }
+    }
 
-      allinfo.push(stats);
+    allinfo.push(stats);
 
-      //console.log(`${nombre} emojis:`);
-      //console.log(contarEmojis(mensajes));
+    //console.log(`${nombre} emojis:`);
+    //console.log(contarEmojis(mensajes));
 
-      /*div.textContent = `${nombre} - total: ${mensajes.length} - rol: ${msjrol} - offrol: ${mjsoff} - media: ${spam}`;
+    /*div.textContent = `${nombre} - total: ${mensajes.length} - rol: ${msjrol} - offrol: ${mjsoff} - media: ${spam}`;
       div.addEventListener('click', ()=> {details(stats)})*/
-      }
+  }
 
-      const organizedarray = allinfo.sort((a, b) => b.rolnumber - a.rolnumber);
-      for (const stats of organizedarray) {
-        const div = document.createElement("div");
-        div.classList.add("mensajediv");
+  const organizedarray = allinfo.sort((a, b) => b.rolnumber - a.rolnumber);
+  let i = 1;
+  for (const stats of organizedarray) {
+    const div = document.createElement("div");
+    div.classList.add("mensajediv");
 
-        div.innerHTML = `
+    div.innerHTML = `
         <p><b>${stats.rolnumber}</b> ${stats.uss}</p>
-        <p>Total: ${stats.off.length + stats.rol.length + stats.spam.length} - Off: ${stats.off.length + stats.spam.length} - Bonus: ${stats.bonus}</p>
-        `
-        
-        div.addEventListener('click', ()=> {details(stats)})
-        divmsj.appendChild(div);
-      }
+        <p>Total: ${
+          stats.off.length + stats.rol.length + stats.spam.length
+        } - Off: ${stats.off.length + stats.spam.length} - Bonus: ${
+      stats.bonus
+    }</p>
+        `;
 
-      statsbtn.addEventListener(("click"), () => stats(allinfo));
+    div.addEventListener("click", () => {
+      details(stats);
+    });
+    divmsj.appendChild(div);
+    i++;
+  }
 }
 
 function separarMensajes(chatExport, date) {
-
-  const chatfiltered = chatExport.filter(mensaje => mensaje.startsWith(date)); 
+  const chatfiltered = chatExport.filter((mensaje) => mensaje.startsWith(date));
   const primerElemento = chatfiltered[0];
   const poselemento = chatExport.indexOf(primerElemento);
 
-  const chatfinal = chatExport.slice((poselemento-1) + 1);
+  const chatfinal = chatExport.slice(poselemento - 1 + 1);
 
-    const mensajesPorPersona = {};
-    for (const mensaje of chatfinal) {
-      const posraya = mensaje.indexOf("-");
-      const texto = mensaje.slice(posraya + 1 );
-      const datehour = mensaje.slice(0, posraya);
+  const mensajesPorPersona = {};
+  for (const mensaje of chatfinal) {
+    const posraya = mensaje.indexOf("-");
+    const texto = mensaje.slice(posraya + 1);
+    const datehour = mensaje.slice(0, posraya);
 
-      const pospuntos = texto.indexOf(":");
-      const msjtexto = texto.slice(pospuntos + 1 );
-      const nombre = texto.slice(0, pospuntos);
+    const pospuntos = texto.indexOf(":");
+    const msjtexto = texto.slice(pospuntos + 1);
+    const nombre = texto.slice(0, pospuntos);
 
-      const poscoma = datehour.indexOf(",");
-      const hora = datehour.slice(poscoma + 1 );
-      const fecha = datehour.slice(0, poscoma);
+    const poscoma = datehour.indexOf(",");
+    const hora = datehour.slice(poscoma + 1);
+    const fecha = datehour.slice(0, poscoma);
 
-      const mensajefull = {
-        fecha: fecha,
-        hora: hora,
-        mensaje: msjtexto
+    const mensajefull = {
+      fecha: fecha,
+      hora: hora,
+      mensaje: msjtexto,
+    };
+
+    if (nombre) {
+      if (!mensajesPorPersona[nombre]) {
+        mensajesPorPersona[nombre] = [];
       }
-
-      if (nombre) {
-        if (!mensajesPorPersona[nombre]) {
-          mensajesPorPersona[nombre] = [];
-        }
-        mensajesPorPersona[nombre].push(mensajefull);
-      }
+      mensajesPorPersona[nombre].push(mensajefull);
     }
-    return mensajesPorPersona;
+  }
+  return mensajesPorPersona;
 }
 
 function ignorarMensajesWhatsApp(objeto) {
-    const objetoNuevo = {};
+  const objetoNuevo = {};
   for (const key in objeto) {
     const array = objeto[key];
-    if (array.length > 1 && !key.includes("changed this group's settings") && !key.includes("pinned a messag") && !key.includes("requested to join") && !key.match(/୧ ✿ › .* lef/)) {
+    if (
+      array.length > 1 &&
+      !key.includes("changed this group's settings") &&
+      !key.includes("pinned a messag") &&
+      !key.includes("requested to join") &&
+      !key.match(/୧ ✿ › .* lef/)
+    ) {
       objetoNuevo[key] = array;
     }
   }
+
+  console.log(objetoNuevo);
   return objetoNuevo;
 }
 
-
 //este agarra el texto y lo pone en un array así bn bonito
 function counter(texto, date) {
+  console.log(texto);
   // 2024-03-12 => input
   // 3/12/24 => output
 
-  const formattedDate = date.split('-');
+  const formattedDate = date.split("-");
   var day = formattedDate[2];
   var month = formattedDate[1];
   const year = formattedDate[0].slice(2, 4);
@@ -322,55 +408,65 @@ function counter(texto, date) {
 
   //const content = texto.split('CUT_HERE');
 
-  const lines = texto.split('\n');
+  const lines = texto.split("\n");
 
-    let combinedLines = [];
-            let currentMessage = "";
-            for (const line of lines) {
-                if (line.match(/^\d{1,2}\/\d{1,2}\/\d{2,4}, \d{1,2}:\d{1,2}/)) {
-                    // Es una nueva línea de mensaje
-                    if (currentMessage !== "") {
-                        combinedLines.push(currentMessage.trim());
-                    }
-                    currentMessage = line;
-                } else {
-                    // Pertenece al mensaje actual
-                    currentMessage += " " + line;
-                }
-            }
+  let combinedLines = [];
+  let currentMessage = "";
+  for (const line of lines) {
+    if (line.match(/^\d{1,2}\/\d{1,2}\/\d{2,4}, \d{1,2}:\d{1,2}/)) {
+      // Es una nueva línea de mensaje
+      if (currentMessage !== "") {
+        combinedLines.push(currentMessage.trim());
+      }
+      currentMessage = line;
+    } else {
+      // Pertenece al mensaje actual
+      currentMessage += " " + line;
+    }
+  }
 
-            if (currentMessage !== "") {
-                combinedLines.push(currentMessage.trim());
-            }
+  if (currentMessage !== "") {
+    combinedLines.push(currentMessage.trim());
+  }
 
-            const chatLines = combinedLines.join('\n'); // Unir líneas combinadas
+  const chatLines = combinedLines.join("\n"); // Unir líneas combinadas
 
-            const linessplit = chatLines.split('\n');
+  const linessplit = chatLines.split("\n");
 
-            const mjs = ignorarMensajesWhatsApp(separarMensajes(linessplit, dateFormatted));
+  console.log(linessplit);
+  console.log(`Fecha: ${dateFormatted}`);
 
-            printmjs(mjs)
+  const mjs = ignorarMensajesWhatsApp(
+    separarMensajes(linessplit, dateFormatted)
+  );
+
+  console.log(mjs);
+
+  printmjs(mjs);
 }
 
 //acá el input solo manda el texto pa arriba
 
 const forms = document.querySelector("#forms");
 const fileInput = document.querySelector("#file");
+//const fileInput2 = document.querySelector("#file2");
 
 forms.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const dataforms = new FormData(forms);
-    const date =  dataforms.get("date");
-    const file = fileInput.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            content = e.target.result;
-            counter(content, date);
-            };
-        reader.readAsText(file);
-    }
+  e.preventDefault();
+  const dataforms = new FormData(forms);
+  const date = dataforms.get("date");
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      content = e.target.result;
+      counter(content, date);
+    };
+    reader.readAsText(file);
+  }
 });
 
-
-
+function combine(content1, content2, date) {
+  const combinedContent = content1 + "\n" + content2;
+  counter(combinedContent, date);
+}
